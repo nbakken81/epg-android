2da1e3d827c1d8279d3c0bd2032b7a95e3f3a407
lsoares <lsoares@gmail.com>
2014-04-01 23:00:04 +0000

    simplificar!

--- epg/src/pt/mobiledev/tvalarmes/util/Dates.java
+++ epg/src/pt/mobiledev/tvalarmes/util/Dates.java
@@ -5,14 +5,14 @@ import java.util.Date;
 
 public class Dates {
 
-    public static Date subtractDays(int days) {
-        return addDays(-days);
-    }
-
     public static Date addDays(int days) {
         Calendar cal = Calendar.getInstance();
         cal.add(Calendar.DATE, +days);
         Date endDate = cal.getTime();
         return endDate;
+    }
+
+    public static Date subtractDays(int days) {
+        return addDays(-days);
     }
 }
