389b8fffefae637305490e0ebd81309caa4015bc
lsoares <lsoares@gmail.com>
2014-04-09 12:23:00 +0000

    get canais v0.0.1

--- epg/src/pt/mobiledev/tvalarmes/ChannelsBaseAdapter.java
+++ epg/src/pt/mobiledev/tvalarmes/ChannelsBaseAdapter.java
@@ -1,14 +1,13 @@
 package pt.mobiledev.tvalarmes;
 
-import java.util.ArrayList;
-
-import pt.mobiledev.tvalarmes.domain.Channel;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
+import java.util.ArrayList;
+import pt.mobiledev.tvalarmes.domain.Channel;
 
 public class ChannelsBaseAdapter extends BaseAdapter {
 
@@ -39,11 +38,9 @@ public class ChannelsBaseAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-    	
         convertView = inflater.inflate(R.layout.layout_list_channel, null);
         detail(convertView, R.id.tvTitle, channels.get(position).getName());
         return convertView;
-        
     }
 
     private TextView detail(View v, int resId, String text) {
--- epg/src/pt/mobiledev/tvalarmes/dao/EPGDao.java
+++ epg/src/pt/mobiledev/tvalarmes/dao/EPGDao.java
@@ -1,7 +1,9 @@
 package pt.mobiledev.tvalarmes.dao;
 
 import android.util.Log;
+import android.util.Xml;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -13,6 +15,9 @@ import javax.xml.parsers.SAXParserFactory;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import pt.mobiledev.tvalarmes.domain.Channel;
 import pt.mobiledev.tvalarmes.domain.Program;
 import static pt.mobiledev.tvalarmes.util.API.formatDate;
 import pt.mobiledev.tvalarmes.util.Dates;
@@ -21,15 +26,8 @@ public class EPGDao {
 
     final static String BASE_URL = "http://services.sapo.pt/EPG/";
     final static String GET_CHANNEL_FUNCTION = "GetChannelByDateInterval";
+    final static String GET_CHANNELS_FUNCTION = "GetChannelList";
     final static int daysInterval = 1;
-
-    /* URL Methods */
-    public static String getProgramsURL(String sigla) {
-        return BASE_URL + GET_CHANNEL_FUNCTION
-                + "?channelSigla=" + sigla
-                + "&startDate=" + formatDate(Dates.addDays(daysInterval))
-                + "&endDate=" + formatDate(Dates.subtractDays(daysInterval));
-    }
 
     public static ArrayList<Program> getPrograms(String... siglas) {
         try {
@@ -37,11 +35,14 @@ public class EPGDao {
             SAXParserFactory saxPF = SAXParserFactory.newInstance();
             SAXParser saxP = saxPF.newSAXParser();
             XMLReader xmlR = saxP.getXMLReader();
-            URL url = new URL(getProgramsURL(siglas[0])); // TODO várias
+            URL url = new URL(BASE_URL + GET_CHANNEL_FUNCTION
+                    + "?channelSigla=" + siglas[0]
+                    + "&startDate=" + formatDate(Dates.addDays(daysInterval))
+                    + "&endDate=" + formatDate(Dates.subtractDays(daysInterval))); // TODO vários canais
             Log.v(EPGDao.class.getPackage().toString(), url.toString());
 
             // Create the Handler to handle each of the XML tags
-            XMLHandler myXMLHandler = new XMLHandler();
+            XMLProgramsHandler myXMLHandler = new XMLProgramsHandler();
             xmlR.setContentHandler(myXMLHandler);
             xmlR.parse(new InputSource(url.openStream()));
             return myXMLHandler.getPrograms();
@@ -55,6 +56,16 @@ public class EPGDao {
         } catch (IOException ex) {
             Logger.getLogger(EPGDao.class.getName()).log(Level.SEVERE, null, ex);
         }
+        return null;
+    }
+
+    public static ArrayList<Channel> getChannels() throws IOException, XmlPullParserException {
+        InputStream in = new URL(BASE_URL + GET_CHANNELS_FUNCTION).openStream();
+        XmlPullParser parser = Xml.newPullParser();
+        parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+        parser.setInput(in, null);
+        parser.nextTag();
+        in.close();
         return null;
     }
 
