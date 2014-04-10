bde90017a7db41eb78a60d49c7bf36c80f2b8af2
lsoares <lsoares@gmail.com>
2014-04-10 10:54:47 +0000

    asdsadsadsadsa

--- epg/bin/AndroidApplication1-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-C:\Users\laptop\Documents\NetBeansProjects\epg\trunk\epg\bin\AndroidApplication1-debug-unaligned.apk : \
-C:\Users\laptop\Documents\NetBeansProjects\epg\trunk\epg\bin\AndroidApplication1.ap_ \
-C:\Users\laptop\Documents\NetBeansProjects\epg\trunk\epg\bin\classes.dex \
-C:\Users\laptop\Documents\NetBeansProjects\epg\trunk\epg\libs\android-support-v4.jar \
--- epg/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\laptop\Documents\NetBeansProjects\epg\trunk\epg\bin\AndroidManifest.xml : \
-C:\Users\laptop\Documents\NetBeansProjects\epg\trunk\epg\AndroidManifest.xml \
