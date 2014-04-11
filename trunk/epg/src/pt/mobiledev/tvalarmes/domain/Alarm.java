af329e843ef11a56768e12a803ab1fe9ae9b0c7a
lsoares@gmail.com <lsoares@gmail.com@cfd29202-e421-97e9-c0e6-2b184cd4f1f3>
2014-04-11 00:03:19 +0000

    leitura dos programas uniformizada

--- trunk/epg/src/pt/mobiledev/tvalarmes/domain/Alarm.java
+++ trunk/epg/src/pt/mobiledev/tvalarmes/domain/Alarm.java
@@ -2,4 +2,13 @@ package pt.mobiledev.tvalarmes.domain;
 
 public class Alarm extends Program {
 
+    int minutesBefore;
+
+    public int getMinutesBefore() {
+        return minutesBefore;
+    }
+
+    public void setMinutesBefore(int minutesBefore) {
+        this.minutesBefore = minutesBefore;
+    }
 }
