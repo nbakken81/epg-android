d21e553aa06192e4a5408f219ffdb277c042f517
diogo.tpr <diogo.tpr@gmail.com>
2014-04-12 21:45:18 +0000

    alteracoes no alarme

--- epg/src/pt/mobiledev/tvalarmes/domain/Alarm.java
+++ epg/src/pt/mobiledev/tvalarmes/domain/Alarm.java
@@ -3,12 +3,26 @@ package pt.mobiledev.tvalarmes.domain;
 import java.io.Serializable;
 
 @SuppressWarnings("serial")
-public class Alarm extends Program implements Serializable {
-	
+public class Alarm implements Serializable {
+	Program program;
     int minutesBefore;
     boolean repeats;
 
-    public boolean isRepeats() {
+    public Alarm(Program program, int minutesBefore, boolean repeats) {
+		this.program = program;
+		this.minutesBefore = minutesBefore;
+		this.repeats = repeats;
+	}
+
+	public Program getProgram() {
+		return program;
+	}
+
+	public void setProgram(Program program) {
+		this.program = program;
+	}
+
+	public boolean isRepeats() {
 		return repeats;
 	}
 
