3fdeaf4db18426c5d25c8da7c34889d133dc0b45
lsoares <lsoares@gmail.com>
2014-04-13 03:21:42 +0000

    matching de temporada e episódio

--- epg/src/pt/mobiledev/tvalarmes/util/Util.java
+++ epg/src/pt/mobiledev/tvalarmes/util/Util.java
@@ -1,14 +1,5 @@
 package pt.mobiledev.tvalarmes.util;
 
-import java.text.Normalizer;
-import java.text.Normalizer.Form;
-import java.util.Calendar;
-import java.util.Date;
-
-import pt.mobiledev.tvalarmes.AlarmsActivity;
-import pt.mobiledev.tvalarmes.R;
-import pt.mobiledev.tvalarmes.domain.Alarm;
-import pt.mobiledev.tvalarmes.domain.Program;
 import android.app.AlarmManager;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -17,6 +8,14 @@ import android.content.Intent;
 import android.os.SystemClock;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
+import java.text.Normalizer;
+import java.text.Normalizer.Form;
+import java.util.Calendar;
+import java.util.Date;
+import pt.mobiledev.tvalarmes.AlarmsActivity;
+import pt.mobiledev.tvalarmes.R;
+import pt.mobiledev.tvalarmes.domain.Alarm;
+import pt.mobiledev.tvalarmes.domain.Program;
 
 public class Util {
 
@@ -36,56 +35,55 @@ public class Util {
                 .getIdentifier(resourceName, packageName, context.getPackageName());
         return checkExistence;
     }
-    
+
     public static String removeDiacriticalMarks(String string) {
         return Normalizer.normalize(string, Form.NFD)
                 .replaceAll("\\p{InCombiningDiacriticalMarks}+", "");
     }
-    
+
     public static void createAlarm(Context context, Program program, int minutesBefore, boolean repeats) {
-    	AlarmManager alarmManager;
-    	PendingIntent alarmIntent;
-		// Criar alarme
-		Alarm alarm = new Alarm(program, minutesBefore, repeats);
-    	// Iniciar Alarm Manager
-    	alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
-    	//Criar Intent
-		Intent intent = new Intent(context, AlarmReceiver.class);
-		// Adicionar alarme ao intent
-		intent.putExtra("alarm", alarm);
-		
-		alarmIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
-		// Tocar o alarme dois segundos depois de ser criado
-		alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-		        SystemClock.elapsedRealtime() +
-		        2 * 1000, alarmIntent);
+        AlarmManager alarmManager;
+        PendingIntent alarmIntent;
+        // Criar alarme
+        Alarm alarm = new Alarm(program, minutesBefore, repeats);
+        // Iniciar Alarm Manager
+        alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        //Criar Intent
+        Intent intent = new Intent(context, AlarmReceiver.class);
+        // Adicionar alarme ao intent
+        intent.putExtra("alarm", alarm);
+
+        alarmIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+        // Tocar o alarme dois segundos depois de ser criado
+        alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                SystemClock.elapsedRealtime()
+                + 2 * 1000, alarmIntent);
     }
-    
+
     public static void runNotification(Context context, Alarm alarm) {
-    	String alarmText;
-    	switch (alarm.getMinutesBefore()) {
-		case 0:
-			alarmText = "Está a começar.";
-			break;
-		case 1:
-			alarmText = "Começa dentro de 1 minuto.";
-		default:
-			alarmText = "Começa dentro de " + alarm.getMinutesBefore() + " minutos.";
-			break;
-		}
-    	
-    	NotificationCompat.Builder mBuilder =
-    	        new NotificationCompat.Builder(context)
-    	        .setSmallIcon(R.drawable.axn)
-    	        .setContentTitle(alarm.getProgram().getTitle())
-    	        .setContentText(alarmText);
-    	Intent resultIntent = new Intent(context, AlarmsActivity.class);
-    	TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
-    	stackBuilder.addParentStack(AlarmsActivity.class);
-    	stackBuilder.addNextIntent(resultIntent);
-    	PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
-    	mBuilder.setContentIntent(resultPendingIntent);
-    	NotificationManager mNotificationManager =  (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-    	mNotificationManager.notify(1, mBuilder.build());
+        String alarmText;
+        switch (alarm.getMinutesBefore()) {
+            case 0:
+                alarmText = "Est√° a come√ßar.";
+                break;
+            case 1:
+                alarmText = "Come√ßa dentro de 1 minuto.";
+            default:
+                alarmText = "Come√ßa dentro de " + alarm.getMinutesBefore() + " minutos.";
+                break;
+        }
+        NotificationCompat.Builder mBuilder
+                = new NotificationCompat.Builder(context)
+                .setSmallIcon(R.drawable.axn)
+                .setContentTitle(alarm.getProgram().getTitle())
+                .setContentText(alarmText);
+        Intent resultIntent = new Intent(context, AlarmsActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addParentStack(AlarmsActivity.class);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(1, mBuilder.build());
     }
 }
