000194ad17a4a4ba3dc568c134d84254a46180f2
diogo.tpr <diogo.tpr@gmail.com>
2014-04-25 19:02:29 +0000

    bug da lista

--- epg/src/pt/mobiledev/tvalarmes/ChannelsBaseAdapter.java
+++ epg/src/pt/mobiledev/tvalarmes/ChannelsBaseAdapter.java
@@ -1,5 +1,9 @@
 package pt.mobiledev.tvalarmes;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import pt.mobiledev.tvalarmes.domain.Channel;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,59 +11,65 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-import java.util.ArrayList;
-import java.util.List;
-import pt.mobiledev.tvalarmes.domain.Channel;
 
 public class ChannelsBaseAdapter extends BaseAdapter {
 
-    List<Channel> channels;
-    LayoutInflater inflater;
-    Context context;
+	List<Channel> channels;
+	LayoutInflater inflater;
+	Context context;
 
-    public ChannelsBaseAdapter(final Context context, List<Channel> channels) {
-        this.channels = new ArrayList<Channel>(channels);
-        this.context = context;
-        inflater = LayoutInflater.from(this.context);
-    }
+	public ChannelsBaseAdapter(final Context context, List<Channel> channels) {
+		this.channels = new ArrayList<Channel>(channels);
+		this.context = context;
+		inflater = LayoutInflater.from(this.context);
+	}
 
-    @Override
-    public int getCount() {
-        return channels.size();
-    }
+	@Override
+	public int getCount() {
+		return channels.size();
+	}
 
-    @Override
-    public Channel getItem(int position) {
-        return channels.get(position);
-    }
+	@Override
+	public Channel getItem(int position) {
+		return channels.get(position);
+	}
 
-    @Override
-    public long getItemId(int arg0) {
-        return 0;
-    }
+	@Override
+	public long getItemId(int arg0) {
+		return 0;
+	}
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        Channel ch = channels.get(position);
-        if (ch.getLogoResourceId(context) > 0) {
-            /* Tem imagem */
-            convertView = inflater.inflate(R.layout.channel_detail, null);
-            ImageView iv = (ImageView) convertView.findViewById(R.id.tvImage);
-            iv.setImageResource(ch.getLogoResourceId(context));
-        } else {
-            /* Mostra título */
-            convertView = inflater.inflate(R.layout.channel_detail_text, null);
-            TextView tv = (TextView) convertView.findViewById(R.id.tvTitle);
-            tv.setText(ch.getName());
-        }
-        return convertView;
-    }
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		Channel ch = channels.get(position);
+		boolean hasImage = ch.getLogoResourceId(context) > 0;
+		if(convertView == null) {
+			// Inicializar View
+			convertView = inflater.inflate(R.layout.channel_detail, null);
+		} 
+		// Alterar image/texto da View
+		ImageView iv = (ImageView) convertView.findViewById(R.id.tvImage);
+		TextView tv = (TextView) convertView.findViewById(R.id.tvTitle);
+		if (hasImage) {
+			/* Tem imagem */
+			iv.setImageResource(ch.getLogoResourceId(context));
+			tv.setVisibility(View.GONE);
+			iv.setVisibility(1);
+		} else {
+			/* Mostra título */
+			tv.setText(ch.getName());
+			iv.setVisibility(View.GONE);
+			tv.setVisibility(1);
+		}
 
-    public List<Channel> getChannels() {
-        return channels;
-    }
+		return convertView;
+	}
 
-    public void setChannels(List<Channel> channels) {
-        this.channels = channels;
-    }
+	public List<Channel> getChannels() {
+		return channels;
+	}
+
+	public void setChannels(List<Channel> channels) {
+		this.channels = channels;
+	}
 }
