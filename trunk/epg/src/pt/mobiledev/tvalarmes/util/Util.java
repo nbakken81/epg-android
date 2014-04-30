ceaac91aed512980f7104ce838043a9cc5f05265
lsoares@gmail.com <lsoares@gmail.com@cfd29202-e421-97e9-c0e6-2b184cd4f1f3>
2014-04-30 16:04:10 +0000

    simplific

--- trunk/epg/src/pt/mobiledev/tvalarmes/util/Util.java
+++ trunk/epg/src/pt/mobiledev/tvalarmes/util/Util.java
@@ -1,8 +1,6 @@
 package pt.mobiledev.tvalarmes.util;
 
 import android.content.Context;
-import java.text.Normalizer;
-import java.text.Normalizer.Form;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -22,18 +20,5 @@ public class Util {
         int checkExistence = context.getResources()
                 .getIdentifier(resourceName, packageName, context.getPackageName());
         return checkExistence;
-    }
-
-    public static String removeDiacriticalMarks(String string) {
-        return Normalizer.normalize(string, Form.NFD)
-                .replaceAll("\\p{InCombiningDiacriticalMarks}+", "");
-    }
-
-    public static boolean relaxedStartsWith(String s1, String s2) {
-        return removeDiacriticalMarks(s1.trim().toLowerCase()).startsWith(removeDiacriticalMarks(s2.trim().toLowerCase()));
-    }
-
-    public static boolean relaxedContains(String s1, String s2) {
-        return removeDiacriticalMarks(s1.trim().toLowerCase()).contains(removeDiacriticalMarks(s2.trim().toLowerCase()));
     }
 }
