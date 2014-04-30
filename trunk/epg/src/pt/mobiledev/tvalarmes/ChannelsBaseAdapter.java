b7781dd02870e9923cbd019cd26c0114d01ce50f
lsoares@gmail.com <lsoares@gmail.com@cfd29202-e421-97e9-c0e6-2b184cd4f1f3>
2014-04-30 16:00:50 +0000

    grande refactoring

--- trunk/epg/src/pt/mobiledev/tvalarmes/ChannelsBaseAdapter.java
+++ trunk/epg/src/pt/mobiledev/tvalarmes/ChannelsBaseAdapter.java
@@ -1,75 +1,35 @@
 package pt.mobiledev.tvalarmes;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import pt.mobiledev.tvalarmes.domain.Channel;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.TextView;
+import java.util.List;
+import pt.mobiledev.tvalarmes.domain.Channel;
 
-public class ChannelsBaseAdapter extends BaseAdapter {
+public class ChannelsBaseAdapter extends ArrayAdapter<Channel> {
 
-	List<Channel> channels;
-	LayoutInflater inflater;
-	Context context;
+    LayoutInflater inflater;
 
-	public ChannelsBaseAdapter(final Context context, List<Channel> channels) {
-		this.channels = new ArrayList<Channel>(channels);
-		this.context = context;
-		inflater = LayoutInflater.from(this.context);
-	}
+    public ChannelsBaseAdapter(final Context context, int resource, List<Channel> channels) {
+        super(context, resource, channels);
+        inflater = LayoutInflater.from(context);
+    }
 
-	@Override
-	public int getCount() {
-		return channels.size();
-	}
-
-	@Override
-	public Channel getItem(int position) {
-		return channels.get(position);
-	}
-
-	@Override
-	public long getItemId(int arg0) {
-		return 0;
-	}
-
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		Channel ch = channels.get(position);
-		boolean hasImage = ch.getLogoResourceId(context) > 0;
-		if(convertView == null) {
-			// Inicializar View
-			convertView = inflater.inflate(R.layout.channel_detail, null);
-		} 
-		// Alterar image/texto da View
-		ImageView iv = (ImageView) convertView.findViewById(R.id.tvImage);
-		TextView tv = (TextView) convertView.findViewById(R.id.tvTitle);
-		if (hasImage) {
-			/* Tem imagem */
-			iv.setImageResource(ch.getLogoResourceId(context));
-			tv.setVisibility(View.GONE);
-			iv.setVisibility(1);
-		} else {
-			/* Mostra título */
-			tv.setText(ch.getName());
-			iv.setVisibility(View.GONE);
-			tv.setVisibility(1);
-		}
-
-		return convertView;
-	}
-
-	public List<Channel> getChannels() {
-		return channels;
-	}
-
-	public void setChannels(List<Channel> channels) {
-		this.channels = channels;
-	}
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Channel ch = getItem(position);
+        boolean hasImage = ch.getLogoResourceId(getContext()) > 0;
+        if (convertView == null) {  // Inicializar View
+            convertView = inflater.inflate(R.layout.channel_detail, null);
+        }
+        // Alterar image/texto da View
+        ImageView iv = (ImageView) convertView.findViewById(R.id.tvImage);
+        if (hasImage) {
+            iv.setImageResource(ch.getLogoResourceId(getContext()));
+        }
+        return convertView;
+    }
 }
