2d08a1efda41e54c205459806e1eb914b9221790
lsoares <lsoares@gmail.com>
2014-04-30 18:23:27 +0000

    simplific

--- epg/src/pt/mobiledev/tvalarmes/AlarmsActivity.java
+++ epg/src/pt/mobiledev/tvalarmes/AlarmsActivity.java
@@ -2,9 +2,7 @@ package pt.mobiledev.tvalarmes;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ListView;
 import java.util.List;
 import pt.mobiledev.tvalarmes.dao.AlarmDao;
@@ -16,15 +14,14 @@ public class AlarmsActivity extends Activity {
     ListView lvAlarms;
     Context context;
     List<Alarm> alarms;
-    AlarmDao db;
+    AlarmDao alarmDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.alarm_list);
-
         context = getApplicationContext();
-        db = new AlarmDao(this); // Sacar a base de dados
+        alarmDao = new AlarmDao(this); // Sacar a base de dados
         AlarmNotifier.backgroundTaskScheduler(this); // Agendamento da tarefa de actualização de alarmes em background
     }
 
@@ -32,12 +29,7 @@ public class AlarmsActivity extends Activity {
     public void onResume() {
         super.onResume();
         lvAlarms = (ListView) findViewById(R.id.lvAlarms); // Listview de alarmes
-        AlarmsBaseAdapter alarmsAdapter = new AlarmsBaseAdapter(context, R.layout.alarm_detail, db.findAll(), db);
+        AlarmsBaseAdapter alarmsAdapter = new AlarmsBaseAdapter(context, R.layout.alarm_detail, alarmDao.findAll(), alarmDao);
         lvAlarms.setAdapter(alarmsAdapter);
-    }
-
-    public void goToChannels(View view) {
-        Intent intent = new Intent(this, ChannelsActivity.class);
-        startActivity(intent);
     }
 }
