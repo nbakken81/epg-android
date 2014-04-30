f7dc7d2515ee59dda724173f89d0ee816d31d321
lsoares <lsoares@gmail.com>
2014-04-30 15:49:32 +0000

    grande refactoring

--- epg/src/pt/mobiledev/tvalarmes/ProgramsBaseAdapter.java
+++ epg/src/pt/mobiledev/tvalarmes/ProgramsBaseAdapter.java
@@ -4,59 +4,32 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
-import java.util.ArrayList;
 import java.util.List;
 import pt.mobiledev.tvalarmes.domain.Program;
 
-public class ProgramsBaseAdapter extends BaseAdapter {
+public class ProgramsBaseAdapter extends ArrayAdapter<Program> {
 
-	List<Program> programs;
-	LayoutInflater inflater;
-	Context context;
+    LayoutInflater inflater;
 
-	public ProgramsBaseAdapter(Context context, List<Program> programs) {
-		this.programs = new ArrayList<Program>(programs);
-		this.context = context;
-		inflater = LayoutInflater.from(this.context);
-	}
+    public ProgramsBaseAdapter(Context context, int resource, List<Program> programs) {
+        super(context, resource, programs);
+        inflater = LayoutInflater.from(context);
+    }
 
-	@Override
-	public int getCount() {
-		return programs.size();
-	}
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.program_detail, null);
+        }
+        detail(convertView, R.id.tvTitle, getItem(position).getTitle());
+        return convertView;
+    }
 
-	@Override
-	public Program getItem(int position) {
-		return programs.get(position);
-	}
-
-	@Override
-	public long getItemId(int arg0) {
-		return 0;
-	}
-
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		if (convertView == null) {
-			convertView = inflater.inflate(R.layout.program_detail, null);
-		} 
-		detail(convertView, R.id.tvTitle, programs.get(position).getTitle());
-		return convertView;
-	}
-
-	private TextView detail(View v, int resId, String text) {
-		TextView tv = (TextView) v.findViewById(resId);
-		tv.setText(text);
-		return tv;
-	}
-
-	public List<Program> getPrograms() {
-		return programs;
-	}
-
-	public void setPrograms(List<Program> programs) {
-		this.programs = programs;
-	}
+    private TextView detail(View v, int resId, String text) {
+        TextView tv = (TextView) v.findViewById(resId);
+        tv.setText(text);
+        return tv;
+    }
 }
