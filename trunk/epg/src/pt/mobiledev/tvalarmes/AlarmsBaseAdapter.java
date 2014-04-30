3cef1c9cb4fd2680ffedd77eee1d2b0b4112495d
lsoares@gmail.com <lsoares@gmail.com@cfd29202-e421-97e9-c0e6-2b184cd4f1f3>
2014-04-30 17:54:56 +0000

    simplific

--- trunk/epg/src/pt/mobiledev/tvalarmes/AlarmsBaseAdapter.java
+++ trunk/epg/src/pt/mobiledev/tvalarmes/AlarmsBaseAdapter.java
@@ -5,60 +5,40 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import java.util.ArrayList;
 import java.util.List;
 import pt.mobiledev.tvalarmes.dao.AlarmDao;
 import pt.mobiledev.tvalarmes.domain.Alarm;
 import pt.mobiledev.tvalarmes.domain.Channel;
 
-public class AlarmsBaseAdapter extends BaseAdapter {
+public class AlarmsBaseAdapter extends ArrayAdapter<Alarm> {
 
-    ArrayList<Alarm> alarms = new ArrayList<Alarm>();
     LayoutInflater inflater;
-    Context context;
     AlarmDao alarmDao;
 
-    public AlarmsBaseAdapter(Context context, List<Alarm> alarms, AlarmDao alarmDao) {
-        this.alarms = new ArrayList<Alarm>(alarms);
-        this.context = context;
+    public AlarmsBaseAdapter(Context context, int resId, List<Alarm> alarms, AlarmDao alarmDao) {
+        super(context, resId, alarms);
         this.alarmDao = alarmDao;
-        inflater = LayoutInflater.from(this.context);
-    }
-
-    @Override
-    public int getCount() {
-        return alarms.size();
-    }
-
-    @Override
-    public Alarm getItem(int position) {
-        return alarms.get(position);
-    }
-
-    @Override
-    public long getItemId(int arg0) {
-        return 0;
+        inflater = LayoutInflater.from(context);
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-    	if (convertView == null) {
-    		convertView = inflater.inflate(R.layout.alarm_detail, null);
-    	}
-        detail(convertView, alarms.get(position));
-        Button btnDelete = (Button) convertView.findViewById(R.id.btnDeleteAlarm);
-        btnDelete.setTag(position);
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.alarm_detail, null);
+        }
+        detail(convertView, getItem(position));
+        Button btnDelete = (Button) convertView.findViewById(R.id.btnDeleteAlarm);        btnDelete.setTag(position);
         btnDelete.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 int position = (Integer) v.getTag();
-                Alarm alarm = alarms.get(position);
+                Alarm alarm = getItem(position);
                 alarmDao.delete(alarm);  // Apagar da base de dados
-                alarms.remove(position);  // Apagar da lista
+                remove(alarm);  // Apagar da lista
                 notifyDataSetChanged();  // Actualizar lista
             }
         });
@@ -69,6 +49,6 @@ public class AlarmsBaseAdapter extends BaseAdapter {
         TextView tv = (TextView) v.findViewById(R.id.tvTitle);
         tv.setText(alarm.getProgram().getTitle());
         ImageView pic = (ImageView) v.findViewById(R.id.tvImageAlarm);
-        pic.setImageResource(new Channel(alarm.getProgram().getChannelId()).getLogoResourceId(context));
+        pic.setImageResource(new Channel(alarm.getProgram().getChannelId()).getLogoResourceId(getContext()));
     }
 }
